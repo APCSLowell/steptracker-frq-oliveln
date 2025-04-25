3a04f763e36f873395e88809dcd5e09569cc205d
oliveln <143148891+oliveln@users.noreply.github.com>
2025-04-24 21:01:08 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,38 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
-} 
+    private final int minStepsActive;
+    private int activeDays;
+    private int days;
+    private int totalSteps;
+
+    public StepTracker(int minStepsActive)
+    {
+        this.minStepsActive = minStepsActive;
+        activeDays = 0;
+        days = 0;
+        totalSteps = 0;
+    }
+
+    public void addDailySteps(int steps)
+    {
+        if(steps >= minStepsActive)
+            activeDays++;
+
+        days++;
+        totalSteps += steps;
+    }
+
+    public int activeDays()
+    {
+        return activeDays;
+    }
+
+    public double averageSteps()
+    {
+        if(days == 0)
+            return 0;
+
+        return totalSteps / (double) days;
+    }
+}
